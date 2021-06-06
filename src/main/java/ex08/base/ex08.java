d5240583797bad095234befe5ff6ac6101824cc4
rachelsch1 <schwarz.rachel.e@gmail.com>
2021-06-06 12:14:58 -0400

    initial solution

--- src/main/java/ex08/base/ex08.java
+++ src/main/java/ex08/base/ex08.java
@@ -1,4 +1,31 @@
 package ex08.base;
 
+import java.util.Scanner;
+
 public class ex08 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("How many people? ");
+        String str1 = input.nextLine();
+        int numPeople = Integer.parseInt(str1);
+
+        System.out.print("How many pizzas do you have? ");
+        String str2 = input.nextLine();
+        int numPizzas = Integer.parseInt(str2);
+
+        System.out.print("How many slices per pizza? ");
+        String str3 = input.nextLine();
+        int numSlices = Integer.parseInt(str3);
+
+        int totalSlices = numSlices * numPizzas;
+        System.out.println(numPeople + " with " + numPizzas + " pizzas (" + totalSlices + " slices)");
+
+        int slicesPer = totalSlices / numPeople;
+        int leftOver = totalSlices % numPeople;
+
+        System.out.println("Each person gets " + slicesPer + " slices of pizza.");
+        System.out.println("There are " + leftOver + " left over pieces. ");
+
+    }
 }
